d5b966a58bec23a795eae7c8e4a4e829e539dfaf
YusefB <yusefspam@gmail.com>
2020-09-13 11:56:10 -0400

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,24 @@
+#Yusef Bayyat
+#8/31/20
+#the object of this code is to print a turtle graph of a hexagon
+#Turtle graph
+#Hexagon
+import turtle
+
+def main():
+
+    Ali = turtle.Turtle()
+
+    for side in range(6):
+
+        Ali.forward(90)
+
+        Ali.left(300)
+
+        Ali.color('black')
+
+main()
+
+
+
+
